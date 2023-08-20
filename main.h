7f70ea5aa76253a334064a5aeae7427372c5b8e9
Rotimi <rotimi.omitoogun@gmail.com>
2023-08-20 11:12:08 -0700

    Added function that converts to base 16 and edited the header file

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int specifier_checker(char specifier, va_list arg_list);
+int print_x(unsigned int n, int base);
 
-#endif
+#endif /* MAIN_H*/
