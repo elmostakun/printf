caf7d9c84989084f10fd9543c52c4e0d61a99ff2
Rotimi <rotimi.omitoogun@gmail.com>
2023-08-21 04:49:49 -0700

    major changes

--- _printf.c
+++ _printf.c
@@ -9,30 +9,20 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_list;
-	/* Here, arg_list variable of type va_list is declared*/
-
-	int char_printed_count;
-	/* char_printed_count will store count of character printed*/
-
-	int i;
-	/* i will be used as an iterator through content of format (variable)*/
+	int char_printed_count, i;
 
 	va_start(arg_list, format);
-	/* Here, the arg_list is directed to point to next argument after format*/
-
 	char_printed_count = 0;
 
+	if (format == NULL)
+		return (-1);
 	for (i = 0; format && format[i] != '\0'; i++)
 	/*checking if format is not null and charact at index i not null*/
 	{
-		if (format[i] == '%')
-			/*check if current value at index i in format variable is %*/
-			char_printed_count += specifier_checker(format[++i], arg_list);
-			/* if it is %,call this function to check specifier type*/
-		else
+		if (format[i] != '%')
 			char_printed_count += write(1, format, 1);
-		/*if current character not %, print it out*/
-		/*this process above goes on until nothing is left in format i.e NULL*/
+		else
+			char_printed_count += specifier_checker(format[++i], arg_list);
 	}
 	va_end(arg_list);
 
--- functions.c
+++ functions.c
@@ -2,35 +2,28 @@
 
 /**
  * print_c - function to print char
- * @arg_list: list
+ * @c: char to print
  * Return:characters printed
  */
 
-int print_c(va_list arg_list)
+int print_c(int c)
 {
-char c = va_arg(arg_list, int);
 return (write(1, &c, 1));
 }
 
 /**
  * print_s - function to print string
- * @arg_list: list
+ * @s: string
  * Return: characters printed
  */
 
-int print_s(va_list arg_list)
+int print_s(char *s)
 {
 int i = 0;
-char *str;
-if (str == NULL)
-{
-_putchar("(null)");
-return (i);
-}
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
+
+while (*str != '\0')
+print_c((int)*s);
+++i;
+++s;
 return (i);
 }
