9dd5b0e5b10f88fef65e782121996466457fdff4
Rotimi <rotimi.omitoogun@gmail.com>
2023-08-21 03:23:26 -0700

    changes to header and functions file

--- main.h
+++ main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-int specifier_checker(char specifier, va_list arg_list);
+int specifier_checker(char spec_form, va_list arg_list);
 int print_x(unsigned int n, int base);
 
 #endif /* MAIN_H*/
--- print_i.c
+++ print_i.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * print_xc - converts a number to base 16
- * but the letters are capitalized
+ * print_d - converts a number to base 10
  * @n: number to convert
- * @base: base 16
- * Return: the conversion in base 16
+ * @base: base 10
+ * Return: the conversion in base 10
  */
 
-int print_xc(unsigned int n, int base)
+int print_d(unsigned int n, int base)
 {
 	int div_count = 0;
 	/*Here, this variable is declared to hold a portion*/
 	/*of division that will occur if the figure is > the base*/
-	/*just like 18 > 16 as the base so 1 will be saved in div_count*/
-	/*as 18 divided by 16 is 1 then we have remainder*/
+	/*just like 11 > 10 as the base so 1 will be saved in div_count*/
+	/*as 11 divided by 10 is 1 then we have remainder*/
 
 	char *converter;
 	/*a variable to store lookup table for conversion to base 16*/
 
-	converter = "0123456789ABCDEF";
+	converter = "0123456789";
 	/*here we have the lookup table stored in the variable*/
 
 	if (n < base)
@@ -27,7 +26,7 @@ int print_xc(unsigned int n, int base)
 		/*the if above checks if the number to*/
 		/*convert is less than base to convert to*/
 		/*but not negative number*/
-		return (print_xc(converter[n]));
+		return (print_d(converter[n]));
 		/* number is less, we select its index from lookup table*/
 		/*and return it*/
 	}
@@ -35,14 +34,14 @@ int print_xc(unsigned int n, int base)
 	else
 	{
 		/*if the value is > base, we firt do the below*/
-		div_count = print_xc((n / base), base);
+		div_count = print_d((n / base), base);
 		/*here we get calculate the integer side of the division like*/
-		/* to convert 18 to base 16, we have just 1 of 16 in 18*/
+		/* to convert 11 to base 10, we have just 1 of 10 in 11*/
 		/*then we have 2 remaining after the division*/
 		/*the integer side is stored in div-count*/
 		/*the remainder side is stored in (n % base)*/
 
-		return (div_count + print_xc((n % base), base));
+		return (div_count + print_d((n % base), base));
 		/*we return the calculation here*/
 	}
 }
